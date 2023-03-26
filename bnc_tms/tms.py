b65e32623bad8f53001bba0c59df3b58ac81d0d3
Francis Fregeau <frankfredj@gmail.com>
2023-03-26 12:55:59 -0400

    switched to Lasso

--- bnc_tms/tms.py
+++ bnc_tms/tms.py
@@ -1,18 +1,20 @@
-import numpy as np
-import pandas as pd
+"""tms module"""
+
 import math
 import scipy
-
+import numpy as np
+import pandas as pd
+from sklearn import linear_model
 from typing import Dict, Union, Any
 
 
 def extract_axes(
-    df: pd.DataFrame, date_col: str = "Date", value_col: str = "Rows"
-) -> Union[Dict[str, np.array], None]:
+    tms_df: pd.DataFrame, date_col: str = "Date", value_col: str = "Rows"
+) -> Union[Dict[str, Any], None]:
     """Returns a t and y axis taken from a pandas DataFrame containing a date and a value column.
 
     Args:
-        df: a pandas DataFrame
+        tms_df: a pandas DataFrame
         date_col: name of a the column containing the date or timestamps
         value_col: name of the column containing the time series
 
@@ -20,15 +22,15 @@ def extract_axes(
         A dictionary containing the time series axes
 
     """
-    if date_col not in df.columns or value_col not in df.columns:
-        print(f"ERROR: column(s) not found in {df.columns}")
+    if date_col not in tms_df.columns or value_col not in tms_df.columns:
+        print(f"ERROR: column(s) not found in {tms_df.columns}")
         return None
 
-    df = df.sort_values([date_col], ascending=True).reset_index(drop=True)
+    tms_df = tms_df.sort_values([date_col], ascending=True).reset_index(drop=True)
 
     try:
-        min_date = df[date_col].min()
-        t = np.array([(x - min_date).total_seconds() / 86400 for x in df[date_col]])
+        min_date = tms_df[date_col].min()
+        t = np.array([int((x - min_date).total_seconds() / 60) for x in tms_df[date_col]])  # pylint: disable=C0103,R0915
 
     except ValueError:
         print(
@@ -36,104 +38,137 @@ def extract_axes(
         )
         return None
 
-    y = np.array(df[value_col])
-    return {"t": t, "y": y, "y_start" : min_date}
+    y = np.array(tms_df[value_col])
+    return {"t": t, "y": y, "y_start": min_date}
 
 
 def timeseries_model(
-    df: pd.DataFrame, alpha: float, date_col: str = "Date", value_col: str = "Rows"
-) -> Dict[str, Any]:
+    tms_df: pd.DataFrame,
+    date_col: str = "Date",
+    value_col: str = "Rows",
+    alpha: float = 0.05,
+    use_empirical_residuals_pdf: bool = False,
+) -> Union[Dict[str, Any], None]:  # pylint: disable=R0914
     """Returns the model of type a + b*t + c*sin(phi*t) + d*y_lag.
 
     Args:
-        df: a pandas DataFrame
-        alpha: statistical test alpha
+        tms_df: a pandas DataFrame
         date_col: name of a the column containing the date
         value_col: name of the column containing the time series
+        alpha: statistical test alpha
+        use_empirical_residuals_pdf: is False, residuals are treated as Gaussian
 
     Returns:
         A dictionary containing the model
 
     """
-    axes = extract_axes(df, date_col, value_col)
-    n_points = 2*int(axes["t"][-1])
+    axes = extract_axes(tms_df, date_col, value_col)
+    if not axes:
+        return None
+    
+    last_t = axes["t"][-1]
+    last_y = axes["y"][-1]
 
-    t = np.array(
-        [x for x in np.linspace(start=0, stop=axes["t"][-1], num=n_points + 1)]
-    )
-    y = np.interp(t, axes["t"], axes["y"])
+    # Use the mode of time difference to generate equally spaced points alongside the time domain
+    t_delta = np.diff(axes["t"])
+    t_delta_mode = scipy.stats.mode(t_delta, keepdims=False)[0]
 
-    last_t = t[-1]
-    last_y = y[-1]
-    t = t[:-2]
-    y = y[:-2]
+    # Use interpolation to generate y(t) values
+    n_points = int(axes["t"][-2] / t_delta_mode)
+    t = np.linspace(start=0, stop=axes["t"][-2], num=n_points + 1)
+    y = np.interp(t, axes["t"][:-1], axes["y"][:-1])
 
+    # Detrend y
     X = np.vstack([np.ones(len(t)), t]).T
     c, b = np.linalg.lstsq(X, y, rcond=None)[0]
     y_detrend = y - (b * t + c)
+    y_detrend_cs = (y_detrend - np.mean(y_detrend)) / np.sqrt(np.var(y_detrend))
 
-    acf = np.correlate(y_detrend, y_detrend, "full")[-len(y_detrend) :]
+    # Find the highest frequency
+    acf = np.correlate(y_detrend_cs, y_detrend_cs, "full")[-len(y_detrend) :] / len(
+        y_detrend
+    )
     freq = np.flip(np.argsort(acf))[1]
     t_freq = freq * (t[1] - t[0])
+
+    # Seasonality component for the sinusoidal term
     x_seasonality = 1 + np.sin(t * 2 * math.pi / t_freq)
 
-    has_lag = freq <= len(t) / 2 and t_freq >= 1
-    if has_lag:
+    # Check if lag is reasonable
+    has_lag = freq <= len(t) / 2
 
+    # Lasso regression
+    # Model with lag terms
+    if has_lag:
+        # Values for model with lag and sinunoidal term
         y_final = y[freq:]
         y_lag = y[: (len(y) - freq)]
         t_final = t[freq:]
         x_seasonality_final = x_seasonality[freq:]
 
         X = np.vstack([np.ones(len(t_final)), t_final, x_seasonality_final, y_lag]).T
-        coefs = np.linalg.lstsq(X, y_final, rcond=None)[0]
+        lasso = linear_model.LassoCV(cv=50, random_state=0).fit(X, y_final)
 
-        fitted_val = (
-            coefs[0]
-            + coefs[1] * t_final
-            + coefs[2] * x_seasonality_final
-            + coefs[3] * y_lag
-        )
+        fitted_val = lasso.predict(X)
         residuals = y_final - fitted_val
         r_squared = np.round(np.var(fitted_val) / np.var(y_final), 2)
 
         last_y_lag = np.interp(last_t - t_freq, t, y)
+        E_last_y = lasso.predict(
+            np.array(
+                [1, last_t, (1 + np.sin(last_t * 2 * math.pi / t_freq)), last_y_lag]
+            ).reshape(1, -1)
+        )[0]
 
-        E_last_y = (
-            coefs[0]
-            + coefs[1] * last_t
-            + coefs[2] * (1 + np.sin(last_t * 2 * math.pi / t_freq))
-            + coefs[3] * last_y_lag
-        ) 
+        # If lag terms are insignificant, then act as if there is no seasonality
+        coefs = lasso.coef_
+        has_lag = has_lag and not (coefs[2] == coefs[3] == 0)
 
-    else:
+    # Model with no seasonality
+    if not has_lag:
         X = np.vstack([np.ones(len(t)), t]).T
-        coefs = np.append(np.linalg.lstsq(X, y, rcond=None)[0], np.array([0, 0]))
+        lasso = linear_model.LassoCV(cv=50, random_state=0).fit(X, y)
+        coefs = np.append(lasso.coef_, np.array([0, 0]))
 
-        fitted_val = coefs[0] + coefs[1] * t
+        fitted_val = lasso.predict(X)
         residuals = y - fitted_val
         r_squared = np.round(np.var(fitted_val) / np.var(y), 2)
-        E_last_y = coefs[0] + coefs[1] * last_t
 
-    rmse = math.sqrt(np.mean(residuals**2))
-    z_score = (last_y - E_last_y)/rmse
-    p_val = np.round(scipy.stats.norm.sf(abs(z_score))*2, 4)
-    CI = np.round(np.array([-1, 1])*scipy.stats.norm.ppf(1-alpha/2)*rmse + E_last_y, 4)
-    test_result = "FAIL" if p_val <= alpha else "PASS"
+        last_y_lag = np.interp(last_t - t_freq, t, y)
+        E_last_y = lasso.predict(np.array([1, last_t]).reshape(1, -1))[0]
 
-    sin_term = lag_term = np.round(coefs[2], 4) if has_lag else "NULL"
+    if not use_empirical_residuals_pdf:
+        rmse = math.sqrt(np.mean(residuals**2))
+        z_score = (last_y - E_last_y) / rmse
+        p_val = np.round(scipy.stats.norm.sf(abs(z_score)) * 2, 4)
+        CI = np.round(
+            np.array([-1, 1]) * scipy.stats.norm.ppf(1 - alpha / 2) * rmse + E_last_y, 4
+        )
+    else:
+        residue = last_y - E_last_y
+        cdf_val = sum(1 if x >= residue else 0 for x in residuals) / len(residuals)
+        p_val = np.round(cdf_val * 2 if cdf_val <= 0.5 else 2 * (1 - cdf_val), 4)
+        CI = np.round(
+            scipy.stats.mstats.mquantiles((residuals), [alpha / 2, 1 - alpha / 2])
+            + E_last_y,
+            4,
+        )
+
+    sin_term = np.round(coefs[2], 4) if has_lag else "NULL"
     lag_term = np.round(coefs[3], 4) if has_lag else "NULL"
-    lag_value = t_freq if has_lag else "NULL"
+    lag_value = np.round(t_freq / (24 * 60), 4) if has_lag else "NULL"
+    test_result = "FAIL" if p_val <= alpha else "PASS"
+    cdf_type = "Gaussian" if not use_empirical_residuals_pdf else "Empirical"
 
     print("------ DELTA TIMESERIES TEST RESULT ------")
     print(f"Model r-squared: {r_squared}")
-    print("***")
-    print(f"Daily trend: {np.round(coefs[1], 4)}")
+    print(f"Daily trend: {np.round(coefs[1]*(24*60), 4)}")
     print(f"Sinusoidal term: {sin_term}")
-    print(f"Lag term: {lag_term}")
+    print(f"Y lagged term: {lag_term}")
     print(f"Lag (days): {lag_value}")
-    print("***")
+    print("*****")
     print(f"Test alpha: {alpha}")
+    print(f"Residuals distribution: {cdf_type}")
     print(f"Observed volume: {last_y}")
     print(f"Expected volume: {np.round(E_last_y, 4)}")
     print(f"{1-alpha} CI: ({CI[0]}, {CI[1]})")
@@ -141,16 +176,15 @@ def timeseries_model(
     print(f"Result: {test_result}")
     print("------------------------------------------")
 
-
     return {
         "coefs": coefs,
         "r_squared": r_squared,
         "rmse": rmse,
         "freq": t_freq,
-        "has_lag": freq <= len(t) / 2,
-        "z_score": z_score,
-        "p_value" : p_val,
+        "has_lag": has_lag,
+        "p_value": p_val,
         "CI": CI,
         "test_result": test_result,
-        "alpha": alpha
+        "alpha": alpha,
+        "use_empirical_residuals_pdf": use_empirical_residuals_pdf,
     }
